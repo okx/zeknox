54960bc4fe4376e94e6fbc0f469610c65ca15a17
cliff0412 <cliff.yang@okg.com>
2024-01-25 06:13:24 +0000

    Fix lib header

--- src/lib.h
+++ src/lib.h
@@ -2,7 +2,7 @@
 # include <util/rusterror.h>
 # include <ntt/ntt.h>
 
-// void bn128_add(fp_t *result, fp_t *a, fp_t *b);
+extern "C"
 RustError compute_ntt(size_t device_id, fr_t *inout, uint32_t lg_domain_size,
                 Ntt_Types::InputOutputOrder ntt_order,
                 Ntt_Types::Direction ntt_direction,
