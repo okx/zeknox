e85a5111f5492e6a758a180d5f2f92d26efcd5d0
xingqiang.yuan <xingqiang.yuan@okg.com>
2024-01-30 14:52:09 +0800

    add NTT roots of unity used in x1 prover

--- cuda/ntt/parameters/goldilocks.h
+++ cuda/ntt/parameters/goldilocks.h
@@ -5,8 +5,83 @@ const fr_t group_gen_inverse = fr_t(0x249249246db6db6e);
 
 const int S = 32;
 
-
-
+#ifdef __X1_PROVER__
+// copy from https://github.com/okx/goldilocks/blob/f89eb016830f8c4301482d83691aed22e5a92742/src/goldilocks_base_field.cpp#L11
+const fr_t forward_roots_of_unity[S + 1] = {
+    fr_t(0x1),
+    fr_t(18446744069414584320ULL),
+    fr_t(281474976710656ULL),
+    fr_t(16777216ULL),
+    fr_t(4096ULL),
+    fr_t(64ULL),
+    fr_t(8ULL),
+    fr_t(2198989700608ULL),
+    fr_t(4404853092538523347ULL),
+    fr_t(6434636298004421797ULL),
+    fr_t(4255134452441852017ULL),
+    fr_t(9113133275150391358ULL),
+    fr_t(4355325209153869931ULL),
+    fr_t(4308460244895131701ULL),
+    fr_t(7126024226993609386ULL),
+    fr_t(1873558160482552414ULL),
+    fr_t(8167150655112846419ULL),
+    fr_t(5718075921287398682ULL),
+    fr_t(3411401055030829696ULL),
+    fr_t(8982441859486529725ULL),
+    fr_t(1971462654193939361ULL),
+    fr_t(6553637399136210105ULL),
+    fr_t(8124823329697072476ULL),
+    fr_t(5936499541590631774ULL),
+    fr_t(2709866199236980323ULL),
+    fr_t(8877499657461974390ULL),
+    fr_t(3757607247483852735ULL),
+    fr_t(4969973714567017225ULL),
+    fr_t(2147253751702802259ULL),
+    fr_t(2530564950562219707ULL),
+    fr_t(1905180297017055339ULL),
+    fr_t(3524815499551269279ULL),
+    fr_t(7277203076849721926ULL),
+};
+// modular inverse of `forward_roots_of_unity`
+// inverse_roots_of_unity[i] = forward_roots_of_unity[i] ^ (p-2) % p
+// p = 2^64 - 2^32 + 1
+// calculated by sageMath
+const fr_t inverse_roots_of_unity[S + 1] = {
+    fr_t(0x0000000000000001),
+    fr_t(0xffffffff00000000),
+    fr_t(0xfffeffff00000001),
+    fr_t(0xfffffeff00000101),
+    fr_t(0xffefffff00100001),
+    fr_t(0xfbffffff04000001),
+    fr_t(0xdfffffff20000001),
+    fr_t(0x3fffbfffc0),
+    fr_t(0x7f4949dce07bf05d),
+    fr_t(0x4bd6bb172e15d48c),
+    fr_t(0x38bc97652b54c741),
+    fr_t(0x553a9b711648c890),
+    fr_t(0x55da9bb68958caa),
+    fr_t(0xa0a62f8f0bb8e2b6),
+    fr_t(0x276fd7ae450aee4b),
+    fr_t(0x7b687b64f5de658f),
+    fr_t(0x7de5776cbda187e9),
+    fr_t(0xd2199b156a6f3b06),
+    fr_t(0xd01c8acd8ea0e8c0),
+    fr_t(0x4f38b2439950a4cf),
+    fr_t(0x5987c395dd5dfdcf),
+    fr_t(0x46cf3d56125452b1),
+    fr_t(0x909c4b1a44a69ccb),
+    fr_t(0xc188678a32a54199),
+    fr_t(0xf3650f9ddfcaffa8),
+    fr_t(0xe8ef0e3e40a92655),
+    fr_t(0x7c8abec072bb46a6),
+    fr_t(0xe0bfc17d5c5a7a04),
+    fr_t(0x4c6b8a5a0b79f23a),
+    fr_t(0x6b4d20533ce584fe),
+    fr_t(0xe5cceae468a70ec2),
+    fr_t(0x8958579f296dac7a),
+    fr_t(0x16d265893b5b7e85),
+};
+#else
 const fr_t forward_roots_of_unity[S + 1] = {
     fr_t(0x0000000000000001),
     fr_t(0xffffffff00000000),
@@ -42,7 +117,6 @@ const fr_t forward_roots_of_unity[S + 1] = {
     fr_t(0x400a7f755588e659),  // rou_31, where  rou_32^{1<<31} = 1
     fr_t(0x185629dcda58878c),  // rou_32, where  rou_32^{1<<32} = 1
 };
-
 const fr_t inverse_roots_of_unity[S + 1] = {
     fr_t(0x0000000000000001),
     fr_t(0xffffffff00000000),
@@ -78,6 +152,7 @@ const fr_t inverse_roots_of_unity[S + 1] = {
     fr_t(0x95c0ec9a7ab50701),
     fr_t(0x76b6b635b6fc8719),  // rou_inv_32, where rou_inv_32 = rou_32^{-1}
 };
+#endif
 
 const fr_t domain_size_inverse[S + 1] = {
     fr_t(0x0000000000000001),  // 1^{-1}
