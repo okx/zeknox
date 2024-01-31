63caf20fae12bd9a7f57247d5d5368b3ee47adc4
cliff0412 <cliff.yang@okg.com>
2024-01-31 03:44:10 +0000

    add concurrent tests

--- examples/ntt_concurrent.rs
+++ examples/ntt_concurrent.rs
@@ -1,4 +1,4 @@
-#[cfg(not(feature="no_cuda"))]
+#[cfg(not(feature = "no_cuda"))]
 use cryptography_cuda::{iNTT, init_twiddle_factors_rust, ntt_batch, types::*, NTT};
 use plonky2_field::goldilocks_field::GoldilocksField;
 use plonky2_field::polynomial::PolynomialValues;
@@ -15,16 +15,20 @@ fn random_fr() -> u64 {
     fr % 0xffffffff00000001
 }
 
+fn random_fr_n(n: usize) -> Vec<u64> {
+    (0..n).into_iter().map(|_| random_fr()).collect()
+}
+
 fn cpu_fft_concurrent(concurrent_nums: usize, log_ntt_size: usize) {
     let domain_size = 1usize << log_ntt_size;
     let total_size = domain_size * concurrent_nums;
 
-    let mut gpu_buffer: Vec<u64> = (0..total_size).map(|_| random_fr()).collect();
+    let buffer: Vec<u64> = (0..total_size).map(|_| random_fr()).collect();
     let start = std::time::Instant::now();
-    let ret: Vec<PolynomialValues<GoldilocksField>> = (0..concurrent_nums)
+    let _: Vec<PolynomialValues<GoldilocksField>> = (0..concurrent_nums)
         .into_par_iter()
         .map(|idx| {
-            let coeffs = gpu_buffer[idx * domain_size..(idx + 1) * domain_size]
+            let coeffs = buffer[idx * domain_size..(idx + 1) * domain_size]
                 .iter()
                 .map(|i| GoldilocksField::from_canonical_u64(*i))
                 .collect::<Vec<GoldilocksField>>();
@@ -33,9 +37,70 @@ fn cpu_fft_concurrent(concurrent_nums: usize, log_ntt_size: usize) {
             fft(coefficients.clone())
         })
         .collect();
-    println!("total time spend cpu: {:?}", start.elapsed());
+    println!(
+        "cpu fft of nums: {:?}, log_ntt_size: {:?}, total time spend: {:?}",
+        concurrent_nums,
+        log_ntt_size,
+        start.elapsed()
+    );
+}
+
+#[cfg(not(feature = "no_cuda"))]
+fn gpu_fft_concurrent(concurrent_nums: usize, log_ntt_size: usize) {
+    let domain_size = 1usize << log_ntt_size;
+    let gpu_buffers: Vec<Vec<u64>> = (0..concurrent_nums)
+        .into_iter()
+        .map(|_| random_fr_n(domain_size))
+        .collect();
+  
+    let start = std::time::Instant::now();
+    let _: Vec<_> = gpu_buffers
+        .into_par_iter()
+        .map(|mut gpu_buffer| {
+            NTT(0, &mut gpu_buffer, NTTInputOutputOrder::NN)
+        })
+        .collect();
+    println!(
+        "gpu fft of nums: {:?}, log_ntt_size: {:?}, total time spend: {:?}",
+        concurrent_nums,
+        log_ntt_size,
+        start.elapsed()
+    );
+}
+
+
+#[cfg(not(feature = "no_cuda"))]
+fn gpu_fft_batch(batches: usize, log_ntt_size: usize) {
+    let domain_size = 1usize << log_ntt_size;
+
+    let mut gpu_buffer: Vec<u64> = (0..domain_size * batches).map(|_| random_fr()).collect();
+    let start = std::time::Instant::now();
+    ntt_batch(
+        0,
+        &mut gpu_buffer,
+        NTTInputOutputOrder::NN,
+        batches as u32,
+        log_ntt_size,
+    );
+    println!(
+        "gpu fft batch of nums: {:?}, log_ntt_size: {:?}, total time spend: {:?}",
+        batches,
+        log_ntt_size,
+        start.elapsed()
+    );
 }
 
 fn main() {
-    cpu_fft_concurrent(300, 19);
+    let nums = 260;
+    let log_ntt_size = 19;
+    cpu_fft_concurrent(nums, log_ntt_size);
+
+    #[cfg(not(feature = "no_cuda"))]
+    init_twiddle_factors_rust(0, log_ntt_size);
+
+    #[cfg(not(feature = "no_cuda"))]
+    gpu_fft_concurrent(nums, log_ntt_size);
+
+    #[cfg(not(feature = "no_cuda"))]
+    gpu_fft_batch(nums, log_ntt_size);
 }
