1dfb3a428eea9c2743a0e49a9f736bcbcec6d5f6
cliff0412 <cliff.yang@okg.com>
2024-02-22 16:45:20 +0800

    fix cpu transpose bench

--- benches/transpose.rs
+++ benches/transpose.rs
@@ -1,7 +1,16 @@
 extern crate criterion;
 use criterion::{criterion_group, criterion_main, Criterion};
-use cryptography_cuda::{device::memory::HostOrDeviceSlice, init_twiddle_factors_rs, naive_transpose_rev_batch, transpose_rev_batch, types::NTTConfig};
-use plonky2_field::{fft::fft, goldilocks_field::GoldilocksField, polynomial::PolynomialCoeffs, types::{Field, PrimeField64}};
+#[cfg(not(feature = "no_cuda"))]
+use cryptography_cuda::{
+    device::memory::HostOrDeviceSlice, init_twiddle_factors_rs, naive_transpose_rev_batch,
+    transpose_rev_batch, types::NTTConfig,
+};
+use plonky2_field::{
+    fft::fft,
+    goldilocks_field::GoldilocksField,
+    polynomial::PolynomialCoeffs,
+    types::{Field, PrimeField64},
+};
 use rand::random;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
@@ -11,7 +20,8 @@ fn random_fr() -> u64 {
     fr % 0xffffffff00000001
 }
 
-fn bench_transpose_gpu(c: &mut Criterion){
+#[cfg(not(feature = "no_cuda"))]
+fn bench_transpose_gpu(c: &mut Criterion) {
     let mut group = c.benchmark_group("Transpose");
     let LOG_NTT_SIZES: Vec<usize> = (15..=19).collect();
 
@@ -26,11 +36,8 @@ fn bench_transpose_gpu(c: &mut Criterion){
         let mut device_data2: HostOrDeviceSlice<'_, u64> =
             HostOrDeviceSlice::cuda_malloc(total_elements).unwrap();
 
-        
-        
-        let mut input: Vec<u64> = (0..batches*domain_size).map(|_| random_fr()).collect();
-        let _ = device_data.copy_from_host_offset(input.as_mut_slice(), 0, batches*domain_size);
-        
+        let mut input: Vec<u64> = (0..batches * domain_size).map(|_| random_fr()).collect();
+        let _ = device_data.copy_from_host_offset(input.as_mut_slice(), 0, batches * domain_size);
 
         let mut cfg = NTTConfig::default();
         cfg.are_inputs_on_device = true;
@@ -40,17 +47,23 @@ fn bench_transpose_gpu(c: &mut Criterion){
 
         group.sample_size(20).bench_function(
             &format!("Shared-Mem GPU transpose with n of size 2^{}", log_ntt_size),
-            |b| b.iter(||  transpose_rev_batch(
-                DEFAULT_GPU,
-                device_data.as_mut_ptr(), 
-                device_data2.as_mut_ptr(), 
-                log_ntt_size, 
-                cfg.clone())),
+            |b| {
+                b.iter(|| {
+                    transpose_rev_batch(
+                        DEFAULT_GPU,
+                        device_data.as_mut_ptr(),
+                        device_data2.as_mut_ptr(),
+                        log_ntt_size,
+                        cfg.clone(),
+                    )
+                })
+            },
         );
     }
 }
 
-fn bench_naive_transpose_gpu(c: &mut Criterion){
+#[cfg(not(feature = "no_cuda"))]
+fn bench_naive_transpose_gpu(c: &mut Criterion) {
     let mut group = c.benchmark_group("Transpose");
     let LOG_NTT_SIZES: Vec<usize> = (15..=19).collect();
 
@@ -65,11 +78,8 @@ fn bench_naive_transpose_gpu(c: &mut Criterion){
         let mut device_data2: HostOrDeviceSlice<'_, u64> =
             HostOrDeviceSlice::cuda_malloc(total_elements).unwrap();
 
-        
-        
-        let mut input: Vec<u64> = (0..batches*domain_size).map(|_| random_fr()).collect();
-        let _ = device_data.copy_from_host_offset(input.as_mut_slice(), 0, batches*domain_size);
-        
+        let mut input: Vec<u64> = (0..batches * domain_size).map(|_| random_fr()).collect();
+        let _ = device_data.copy_from_host_offset(input.as_mut_slice(), 0, batches * domain_size);
 
         let mut cfg = NTTConfig::default();
         cfg.are_inputs_on_device = true;
@@ -79,17 +89,22 @@ fn bench_naive_transpose_gpu(c: &mut Criterion){
 
         group.sample_size(20).bench_function(
             &format!("Naive GPU transpose with n of size 2^{}", log_ntt_size),
-            |b| b.iter(||  naive_transpose_rev_batch(
-                DEFAULT_GPU,
-                device_data.as_mut_ptr(), 
-                device_data2.as_mut_ptr(), 
-                log_ntt_size, 
-                cfg.clone())),
+            |b| {
+                b.iter(|| {
+                    naive_transpose_rev_batch(
+                        DEFAULT_GPU,
+                        device_data.as_mut_ptr(),
+                        device_data2.as_mut_ptr(),
+                        log_ntt_size,
+                        cfg.clone(),
+                    )
+                })
+            },
         );
     }
 }
 
-fn bench_transpose_cpu(c: &mut Criterion){
+fn bench_transpose_cpu(c: &mut Criterion) {
     let mut group = c.benchmark_group("Transpose");
     let LOG_NTT_SIZES: Vec<usize> = (15..=19).collect();
 
@@ -98,18 +113,15 @@ fn bench_transpose_cpu(c: &mut Criterion){
         let batches = 200;
 
         // let scalars: Vec<u64> = (0..(total_elements)).map(|_| random_fr()).collect();
-        
-        let mut cpu_buffer: Vec<Vec<u64>> = Vec::new();
-        
-        
-        let input: Vec<u64> = (0..batches*domain_size).map(|_| random_fr()).collect();
-        cpu_buffer.push(input);
 
-       
+        let mut cpu_buffer: Vec<Vec<u64>> = Vec::new();
+
+        let input: Vec<u64> = (0..batches * domain_size).map(|_| random_fr()).collect();
+        cpu_buffer.push(input);
 
         group.sample_size(20).bench_function(
             &format!("Naive GPU transpose with n of size 2^{}", log_ntt_size),
-            |b| b.iter(||  transpose(&cpu_buffer, domain_size))
+            |b| b.iter(|| transpose(&cpu_buffer, domain_size)),
         );
     }
 }
@@ -121,6 +133,16 @@ pub fn transpose<T: Send + Sync + Copy>(matrix: &Vec<Vec<T>>, len: usize) -> Vec
         .collect()
 }
 
-
-criterion_group!(ntt_benches, bench_transpose_gpu, bench_naive_transpose_gpu, bench_transpose_cpu);
+#[cfg(not(feature = "no_cuda"))]
+criterion_group!(
+    ntt_benches,
+    bench_transpose_gpu,
+    bench_naive_transpose_gpu,
+    bench_transpose_cpu
+);
+#[cfg(feature = "no_cuda")]
+criterion_group!(
+    ntt_benches,
+    bench_transpose_cpu
+);
 criterion_main!(ntt_benches);
