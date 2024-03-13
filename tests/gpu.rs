16ca586b9fdb2d4d62fe768120521d42f7f16f8b
cliff0412 <cliff.yang@okg.com>
2024-03-13 16:33:32 +0800

    Fix warning

--- tests/gpu.rs
+++ tests/gpu.rs
@@ -2,6 +2,5 @@ use cryptography_cuda::get_number_of_gpus_rs;
 
 #[test]
 fn test_get_number_of_gpus() {
-    let nums = get_number_of_gpus_rs();
-    assert!(nums>=0);
+    let _ = get_number_of_gpus_rs();
 }
