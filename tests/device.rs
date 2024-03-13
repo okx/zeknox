8a5f31d33dcdce1598d46137295ecb7f34005f35
cliff0412 <cliff.yang@okg.com>
2024-03-13 16:29:37 +0800

    Fix warning

--- tests/device.rs
+++ tests/device.rs
@@ -14,6 +14,6 @@ fn test_list_devices_info_rs() {
 
 #[test]
 fn test_get_number_of_gpus() {
-    let nums = get_number_of_gpus_rs();
-    assert!(nums>=0);
+    let _ = get_number_of_gpus_rs();
+    
 }
