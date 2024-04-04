5e49932d4dec296217091b307198af255296528b
Dumi Loghin <dumiloghin@gmail.com>
2024-04-04 16:07:06 +0800

    improve test script

--- run_rust_tests.sh
+++ run_rust_tests.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -e
+
+if [ -z "$NUM_OF_GPUS" ]; then
+    echo "Please set NUM_OF_GPUS environment variable!"
+    exit 1
+fi
+echo "Running the tests on ${NUM_OF_GPUS} GPU(s)."
+
 cargo test --features=gl64 --test device -- test_get_number_of_gpus --exact --nocapture
 cargo test --features=gl64 --test device -- test_list_devices_info_rs --exact --nocapture
 cargo test --features=gl64 --test ntt -- test_ntt_intt_gl64_self_consistency --exact --nocapture
@@ -14,4 +21,7 @@ cargo test --features=gl64 --test ntt -- test_compute_batched_lde --exact --noca
 cargo test --features=gl64 --test ntt -- test_compute_batched_lde_data_on_device --exact --nocapture
 cargo test --features=gl64 --test ntt --  test_transpose_rev --exact --nocapture
 cargo test --features=gl64 --test ntt --  test_naive_transpose_rev --exact --nocapture
-cargo test --features=gl64 --test ntt -- test_compute_batched_lde_multi_gpu_data_on_one_gpu --exact --nocapture
+
+if [ $NUM_OF_GPUS -gt 1 ]; then
+    cargo test --features=gl64 --test ntt -- test_compute_batched_lde_multi_gpu_data_on_one_gpu --exact --nocapture
+fi
