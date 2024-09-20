7db3fab7a18df309b80d24840cff354b91455189
cliff0412 <cliff.yang@okg.com>
2024-09-20 17:30:45 +0800

    rm unused script

--- scripts/copy_right.sh
+++
@@ -1,5 +0,0 @@
-for file in $(find cuda -name "*.cpp" -o -name "*.h" -o -name "*.hpp"); do
-    if ! grep -q "Copyright" "$file"; then
-        echo "// Copyright 2024 OKX\n$(cat "$file")" > "$file"
-    fi
-done
