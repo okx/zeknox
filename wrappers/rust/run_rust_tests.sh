d773225d12f303d9c5ca85bd21239559488bb13a
Dumi Loghin <dumiloghin@gmail.com>
2024-10-03 12:58:47 +0800

    add merkle tree test in Rust

--- wrappers/rust/run_rust_tests.sh
+++ wrappers/rust/run_rust_tests.sh
@@ -19,8 +19,9 @@ cargo test --features=gl64 --test ntt -- test_ntt_batch_on_device --exact --noca
 cargo test --features=gl64 --test ntt -- test_ntt_batch_with_coset --exact --nocapture
 cargo test --features=gl64 --test ntt -- test_compute_batched_lde --exact --nocapture
 cargo test --features=gl64 --test ntt -- test_compute_batched_lde_data_on_device --exact --nocapture
-cargo test --features=gl64 --test ntt --  test_transpose_rev --exact --nocapture
-cargo test --features=gl64 --test ntt --  test_naive_transpose_rev --exact --nocapture
+cargo test --features=gl64 --test ntt -- test_transpose_rev --exact --nocapture
+cargo test --features=gl64 --test ntt -- test_naive_transpose_rev --exact --nocapture
+cargo test --features=gl64 --test merkle_tree -- --exact --nocapture
 
 if [ $NUM_OF_GPUS -gt 1 ]; then
     cargo test --features=gl64 --test ntt -- test_compute_batched_lde_multi_gpu_data_on_one_gpu --exact --nocapture
