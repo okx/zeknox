14ca47978a2704615a49f5580cf62d220095a26b
Dumi Loghin <dumiloghin@gmail.com>
2024-11-04 18:51:40 +0800

    improve build scripts

--- native/build-debug.sh
+++ native/build-debug.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+CAP=`./configure.sh | grep capability | cut -d ' ' -f 3`
+
 mkdir -p build && cd build
 rm -rf ./*
+if [ -z "$CAP" ]; then
 cmake -DCMAKE_BUILD_TYPE=Debug .. -DBUILD_TESTS=ON
-make -j4
+else
+cmake -DCMAKE_BUILD_TYPE=Debug .. -DBUILD_TESTS=ON -DCUDA_ARCH=$CAP
+fi
+make -j
--- native/build-release.sh
+++ native/build-release.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+CAP=`./configure.sh | grep capability | cut -d ' ' -f 3`
+
 mkdir -p build && cd build
 rm -rf ./*
+if [ -z "$CAP" ]; then
 cmake .. -DBUILD_TESTS=ON
-make -j4
+else
+cmake .. -DBUILD_TESTS=ON -DCUDA_ARCH=$CAP
+fi
+make -j
