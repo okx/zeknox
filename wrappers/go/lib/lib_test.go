a315aef55b270f6d3278f38657b01bdeed1f4d22
Dumi Loghin <dumiloghin@gmail.com>
2024-11-19 14:45:58 +0800

    improve go tests

--- wrappers/go/lib/lib_test.go
+++ wrappers/go/lib/lib_test.go
@@ -156,14 +156,8 @@ func TestTransposeRevBatch(t *testing.T) {
 	}
 }
 
-func TestMerkleTreeBuildingOutputGpu(t *testing.T) {
-	leaf := []uint64{8395359103262935841, 1377884553022145855, 2370707998790318766, 3651132590097252162, 1141848076261006345, 12736915248278257710, 9898074228282442027}
-
-	expHash := []uint64{7544909477878586743, 7431000548126831493, 17815668806142634286, 13168106265494210017}
-
+func caseTestMerkleTreeBuildingOutputGpu(t *testing.T, nLeaves int, nCaps int, leaf, expHash []uint64) {
 	leafSize := len(leaf)
-	nLeaves := 4
-	nCaps := nLeaves
 	hashSize := 4
 	nDigests := 2 * (nLeaves - nCaps)
 	capHeight := int(math.Log2(float64(nCaps)))
@@ -187,28 +181,37 @@ func TestMerkleTreeBuildingOutputGpu(t *testing.T) {
 		for i := 0; i < len(expHash); i++ {
 			if caps[4*j+i] != expHash[i] {
 				t.Errorf("MerkleTreeBuildingOutput() = %v, want %v", caps, expHash)
+				return
 			}
 		}
 	}
 }
 
-func TestMerkleTreeBuildingOutputMultiGpu(t *testing.T) {
-	num, err := GetNumberOfGPUs()
-	if err != nil {
-		t.Errorf("GetNumberOfGPUs() error = %v", err)
-	}
-	if num < 2 {
-		t.Skip("not enough GPUs to test LDEBatchMultiGPU")
-	}
-
+func TestMerkleTreeBuildingOutputGpu(t *testing.T) {
 	leaf := []uint64{8395359103262935841, 1377884553022145855, 2370707998790318766, 3651132590097252162, 1141848076261006345, 12736915248278257710, 9898074228282442027}
 
-	expHash := []uint64{7544909477878586743, 7431000548126831493, 17815668806142634286, 13168106265494210017}
-
-	leafSize := len(leaf)
-	nLeaves := num
+	// case 1: nCaps = nLeaves
+	nLeaves := 8
 	nCaps := nLeaves
+	expHash := []uint64{7544909477878586743, 7431000548126831493, 17815668806142634286, 13168106265494210017}
+	caseTestMerkleTreeBuildingOutputGpu(t, nLeaves, nCaps, leaf, expHash)
+
+	// case 2: subtree_leaves_len = 2
+	nLeaves = 8
+	nCaps = 4
+	expHash = []uint64{7320113784629306061, 6779895905614599055, 11363085033710007978, 10376477785629036773}
+	caseTestMerkleTreeBuildingOutputGpu(t, nLeaves, nCaps, leaf, expHash)
+
+	// case 3:
+	nLeaves = 16
+	nCaps = 2
+	expHash = []uint64{15643713602922591410, 15764162833178839023, 7963799186968795376, 10933634831850922421}
+	caseTestMerkleTreeBuildingOutputGpu(t, nLeaves, nCaps, leaf, expHash)
+}
+
+func caseTestMerkleTreeBuildingOutputMultiGpu(t *testing.T, nLeaves int, nCaps int, leaf, expHash []uint64) {
 	hashSize := 4
+	leafSize := len(leaf)
 	nDigests := 2 * (nLeaves - nCaps)
 	capHeight := int(math.Log2(float64(nCaps)))
 
@@ -237,14 +240,38 @@ func TestMerkleTreeBuildingOutputMultiGpu(t *testing.T) {
 	}
 }
 
-func TestMerkleTreeBuildingOutputCpu(t *testing.T) {
+func TestMerkleTreeBuildingOutputMultiGpu(t *testing.T) {
+	num, err := GetNumberOfGPUs()
+	if err != nil {
+		t.Errorf("GetNumberOfGPUs() error = %v", err)
+	}
+	if num < 2 {
+		t.Skip("not enough GPUs to test LDEBatchMultiGPU")
+	}
+
 	leaf := []uint64{8395359103262935841, 1377884553022145855, 2370707998790318766, 3651132590097252162, 1141848076261006345, 12736915248278257710, 9898074228282442027}
 
-	expHash := []uint64{7544909477878586743, 7431000548126831493, 17815668806142634286, 13168106265494210017}
-
-	leafSize := len(leaf)
-	nLeaves := 4
+	// case 1: nCaps = nLeaves
+	nLeaves := num
 	nCaps := nLeaves
+	expHash := []uint64{7544909477878586743, 7431000548126831493, 17815668806142634286, 13168106265494210017}
+	caseTestMerkleTreeBuildingOutputMultiGpu(t, nLeaves, nCaps, leaf, expHash)
+
+	// case 2: subtree_leaves_len = 2
+	nLeaves = 2 * num
+	nCaps = num
+	expHash = []uint64{7320113784629306061, 6779895905614599055, 11363085033710007978, 10376477785629036773}
+	caseTestMerkleTreeBuildingOutputMultiGpu(t, nLeaves, nCaps, leaf, expHash)
+
+	// case 3: 1 subtree per GPU
+	nLeaves = 16 * num
+	nCaps = 2 * num
+	expHash = []uint64{15643713602922591410, 15764162833178839023, 7963799186968795376, 10933634831850922421}
+	caseTestMerkleTreeBuildingOutputMultiGpu(t, nLeaves, nCaps, leaf, expHash)
+}
+
+func caseTestMerkleTreeBuildingOutputCpu(t *testing.T, nLeaves int, nCaps int, leaf, expHash []uint64) {
+	leafSize := len(leaf)
 	hashSize := 4
 	nDigests := 2 * (nLeaves - nCaps)
 	capHeight := int(math.Log2(float64(nCaps)))
@@ -261,7 +288,30 @@ func TestMerkleTreeBuildingOutputCpu(t *testing.T) {
 		for i := 0; i < len(expHash); i++ {
 			if caps[4*j+i] != expHash[i] {
 				t.Errorf("MerkleTreeBuildingOutput() = %v, want %v", caps, expHash)
+				return
 			}
 		}
 	}
+}
+
+func TestMerkleTreeBuildingOutputCpu(t *testing.T) {
+	leaf := []uint64{8395359103262935841, 1377884553022145855, 2370707998790318766, 3651132590097252162, 1141848076261006345, 12736915248278257710, 9898074228282442027}
+
+	// case 1: nCaps = nLeaves
+	nLeaves := 8
+	nCaps := nLeaves
+	expHash := []uint64{7544909477878586743, 7431000548126831493, 17815668806142634286, 13168106265494210017}
+	caseTestMerkleTreeBuildingOutputCpu(t, nLeaves, nCaps, leaf, expHash)
+
+	// case 2: subtree_leaves_len = 2
+	nLeaves = 8
+	nCaps = 4
+	expHash = []uint64{7320113784629306061, 6779895905614599055, 11363085033710007978, 10376477785629036773}
+	caseTestMerkleTreeBuildingOutputCpu(t, nLeaves, nCaps, leaf, expHash)
+
+	// case 3:
+	nLeaves = 16
+	nCaps = 2
+	expHash = []uint64{15643713602922591410, 15764162833178839023, 7963799186968795376, 10933634831850922421}
+	caseTestMerkleTreeBuildingOutputCpu(t, nLeaves, nCaps, leaf, expHash)
 }
