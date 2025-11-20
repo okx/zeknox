2008280e44c56594b8243064d5c7f32607a5acff
Dumi Loghin <dumiloghin@gmail.com>
2025-11-20 10:20:36 +0800

    move scripts to their folder

--- native/scripts/configure.sh
+++ native/scripts/configure.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! [ -d "scripts" ]; then
+    echo -e "Error: scripts directory not found. Please run this script from the \e[44mnative\e[0m directory of the repository."
+    exit 1
+fi
+
 cd utils/deviceQuery
 make
 if ! [ -e deviceQuery ]; then
